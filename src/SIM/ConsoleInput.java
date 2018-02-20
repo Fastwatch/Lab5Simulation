67fbc59232e4a376eccd05ae49359bc7d6db9101
AndrewKrill <andrew@andrewkrill.com>
2018-02-20 12:07:27 -0600

    Added getTime method. Returns HH:mm:ss

--- src/SIM/ConsoleInput.java
+++ src/SIM/ConsoleInput.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 import ATM.ATM;
@@ -15,12 +17,26 @@ import ATM.ATM;
 public class ConsoleInput {
 
 	public static void main(String args[]) {
+		boolean isRunning = true;
 		Scanner scanner = new Scanner(System.in);
+		String input;
 		
-		//asdasdasd
-		
+		while (isRunning) {
+			input = scanner.next();
+			String timeStamp = getTime();
+			System.out.println(input);
+			System.out.println(timeStamp);
+
+		}
+				
 	}
 	
+	private static String getTime() {
+		//Time Formated as HH:MM:SS
+		//That is, Hour, Min, Sec 
+		String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+		return timeStamp;
+	}
 	
 }
  
