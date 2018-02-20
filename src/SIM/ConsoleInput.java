f8f3646fd223d8c8801de38846b708d285e89f71
AndrewKrill <andrew@andrewkrill.com>
2018-02-20 11:42:53 -0600

    Added Console Input to SIM package

---
+++ src/SIM/ConsoleInput.java
@@ -0,0 +1,25 @@
+package SIM;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import ATM.ATM;
+
+/**
+ * @author Andrew Krill atkrill@uwm.edu
+ */
+
+public class ConsoleInput {
+
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+	}
+	
+	
+}
